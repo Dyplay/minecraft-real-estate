abfdfbd136f3603a002c0efc13561d852977db62
Dyplay <71791638+Dyplay@users.noreply.github.com>
2025-02-21 16:10:25 +0100

    Update page.js

--- src/app/uuid/page.js
+++ src/app/uuid/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import UUIDForm from "../components/UUIDForm";
 import { account } from "../../../lib/appwrite";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function UUIDPage() {
   const [user, setUser] = useState(null);
@@ -15,7 +16,7 @@ export default function UUIDPage() {
         const userData = await account.get();
         setUser(userData);
       } catch {
-        router.push("/login"); // Redirect if not logged in
+        toast.error("❌ Failed to fetch user data. Please log in again.");
       }
     }
     checkUser();

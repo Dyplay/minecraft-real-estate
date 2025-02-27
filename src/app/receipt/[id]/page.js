fb834642dd2682eee8511cc7af697e2409cea97c
Dyplay <71791638+Dyplay@users.noreply.github.com>
2025-02-27 18:34:38 +0100

    Update page.js

--- src/app/receipt/[id]/page.js
+++ src/app/receipt/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { db } from "../../../../lib/appwrite";
 import { PDFDownloadLink, Document, Page, Text, View, StyleSheet, Image } from "@react-pdf/renderer";
 import { toast } from "react-toastify";
